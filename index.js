7741b5081751ed1a8521d8ba50a54d8642389e7e
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-02-10 23:16:03 -0500

    started prompts for team manager

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 // node packages required for function of application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer.prompt([
+
+    {
+        type: "input",
+        name: "teamManagersName",
+        message: "Please enter the name of the team manager.",
+    },
+    
+    {
+        type: "input",
+        name: "employeeID",
+        message: "Please enter the ID of the team manager.",
+    },
+
+    {
+        type: "input",
+        name: "emailAddress",
+        message: "Please enter the email address of the team manager.",
+    },
+
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the office number of the new employee.",
+    },
+
+
+
+
+])
