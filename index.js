034a51aebf6964f3a47bb040a3b4ca8062fe4778
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-02-11 23:17:34 -0500

    figuring out class layout

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+
+function teamManager() {
+    
 inquirer.prompt([
 
     {
@@ -25,10 +29,82 @@ inquirer.prompt([
     {
         type: "input",
         name: "officeNumber",
-        message: "Please enter the office number of the new employee.",
+        message: "Please enter the office number of the team manager.",
     },
 
 
 
 
 ])
+
+// run next function depending on which choice is picked after initial prompts.
+
+}
+function engineer() {
+    
+inquirer.prompt([
+
+    {
+        type: "input",
+        name: "teamManagersName",
+        message: "Please enter the name of the engineer.",
+    },
+    
+    {
+        type: "input",
+        name: "employeeID",
+        message: "Please enter the ID of the engineer.",
+    },
+
+    {
+        type: "input",
+        name: "emailAddress",
+        message: "Please enter the email address of the engineer.",
+    },
+
+    {
+        type: "github",
+        name: "gitHubUser",
+        message: "Please enter the office number of the engineer.",
+    },
+
+
+
+
+])
+
+}
+function intern() {
+    
+inquirer.prompt([
+
+    {
+        type: "input",
+        name: "teamManagersName",
+        message: "Please enter the name of the intern.",
+    },
+    
+    {
+        type: "input",
+        name: "employeeID",
+        message: "Please enter the ID of the intern.",
+    },
+
+    {
+        type: "input",
+        name: "emailAddress",
+        message: "Please enter the email address of the inter.",
+    },
+
+    {
+        type: "input",
+        name: "school",
+        message: "Please enter the school of the intern.",
+    },
+
+
+
+
+])
+
+}
